aa03e41deb768ac35d9c2a5136fdaed07bc5cd79
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-07-18 01:24:16 +0530

    fixed git hub route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Pricing from "./pages/Pricing";
